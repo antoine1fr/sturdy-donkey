1427c2a37475dbf96340f24fb5f095b2a2b84840
Antoine Luciani <antoine1fr@gmail.com>
2018-07-05 20:21:00 -0400

    Write helper functions for loading shaders from disk

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,59 @@
 #include <iostream>
 #include <string>
 
+#define CHECK_GL_ERROR assert(glGetError() == GL_NO_ERROR)
+
+GLuint build_shader(GLenum type, const std::string& sources)
+{
+  GLuint shader = glCreateShader(type);
+  CHECK_GL_ERROR;
+  const char* source_ptr = sources.c_str();
+  glShaderSource(shader, 1, &source_ptr, nullptr);
+  CHECK_GL_ERROR;
+  glCompileShader(shader);
+  CHECK_GL_ERROR;
+  char info_log[1000];
+  GLsizei log_length;
+  glGetShaderInfoLog(shader, sizeof(info_log), &log_length, info_log);
+  CHECK_GL_ERROR;
+  if (log_length > 0)
+    std::cout << "Shader build log: " << info_log << "\n";
+  else
+    std::cout << "Shader built.\n";
+  return shader;
+}
+
+GLuint build_vertex_shader(const std::string& sources)
+{
+  return build_shader(GL_VERTEX_SHADER, sources);
+}
+
+GLuint build_fragment_shader(const std::string& sources)
+{
+  return build_shader(GL_FRAGMENT_SHADER, sources);
+}
+
+GLuint link_gpu_program(GLuint vertex_shader, GLuint fragment_shader)
+{
+  GLuint gpu_program = glCreateProgram();
+  CHECK_GL_ERROR;
+  glAttachShader(gpu_program, vertex_shader);
+  CHECK_GL_ERROR;
+  glAttachShader(gpu_program, fragment_shader);
+  CHECK_GL_ERROR;
+  glLinkProgram(gpu_program);
+  CHECK_GL_ERROR;
+  char info_log[1000];
+  GLsizei log_length;
+  glGetProgramInfoLog(gpu_program, sizeof(info_log), &log_length, info_log);
+  CHECK_GL_ERROR;
+  if (log_length > 0)
+    std::cout << "GPU program link log: " << info_log << "\n";
+  else
+    std::cout << "GPU program linked.\n";
+  return gpu_program;
+}
+
 SDL_Window* setup_video()
 {
   SDL_Window* window = SDL_CreateWindow("Pipelined rendering demo", 0, 0, 800,
