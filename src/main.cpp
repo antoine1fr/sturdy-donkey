46c9d52976c731df3b943efdf595112a1a16906f
Antoine Luciani <antoine1fr@gmail.com>
2018-07-07 16:08:08 -0400

    Request OpenGL 4.1 instead of 3.2

--- src/main.cpp
+++ src/main.cpp
@@ -94,8 +94,8 @@ SDL_Window* setup_video()
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
   SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
   SDL_GL_CreateContext(window);
   gl3wInit();
   return window;
