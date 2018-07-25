7b3f5c4a874de797648322461f879dc0be38cee8
Antoine Luciani <antoine1fr@gmail.com>
2018-07-25 19:45:22 -0400

    Rename vertex_uv to uv in simple vertex shader

--- shaders/simple-vs.glsl
+++ shaders/simple-vs.glsl
@@ -1,7 +1,7 @@
 #version 400 core
 
 in vec3 position;
-in vec2 vertex_uv;
+in vec2 uv;
 
 uniform mat4 model;
 uniform mat4 projection;
@@ -12,5 +12,5 @@ out vec2 fragment_uv;
 void main()
 {
   gl_Position = projection * view * model * vec4(position, 1.0);
-  fragment_uv = vertex_uv;
+  fragment_uv = uv;
 }
