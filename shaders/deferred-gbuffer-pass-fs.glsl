d0e907665f0592572b874f9fd5e8335e4367566a
Antoine Luciani <antoine1fr@gmail.com>
2018-12-11 19:35:23 -0500

    Prefix gbuffer members by gbuffer_ in gbuffer shader

--- shaders/deferred-gbuffer-pass-fs.glsl
+++ shaders/deferred-gbuffer-pass-fs.glsl
@@ -6,15 +6,15 @@ uniform mat4 model;
 uniform mat4 view;
 
 in vec2 fragment_uv;
-layout (location = 0) out vec4 albedo;
-layout (location = 1) out vec4 normal;
+layout (location = 0) out vec4 gbuffer_albedo;
+layout (location = 1) out vec4 gbuffer_normal;
 
 void main()
 {
   // just copy input color into the albedo render target
-  albedo = texture(diffuse_texture, fragment_uv);
+  gbuffer_albedo = texture(diffuse_texture, fragment_uv);
   // write view-space normal into the normal render target
-  normal = (view * model * texture(normal_map, fragment_uv));
+  gbuffer_normal = (view * model * texture(normal_map, fragment_uv));
   //albedo = vec4(1.0, 0.0, 1.0, 1.0);
   //normal = vec4(0.0, 1.0, 1.0, 1.0);
 }
