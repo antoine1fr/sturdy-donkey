95b7ac37d3ef0e94df07cf074211cbef8d9f1457
Antoine Luciani <antoine1fr@gmail.com>
2018-12-11 22:22:01 -0500

    Implement driver functions for binding uniforms

--- src/render/gl/Driver.cpp
+++ src/render/gl/Driver.cpp
@@ -7,7 +7,15 @@ namespace gl
 
 const std::vector<Driver::RenderFunction> Driver::render_functions_({
   &Driver::bind_mesh_,
-  &Driver::draw_elements_
+  &Driver::draw_elements_,
+  &Driver::bind_uniform_float,
+  &Driver::bind_uniform_int,
+  &Driver::bind_uniform_vec2,
+  &Driver::bind_uniform_vec3,
+  &Driver::bind_uniform_vec4,
+  &Driver::bind_uniform_mat2,
+  &Driver::bind_uniform_mat3,
+  &Driver::bind_uniform_mat4
 });
 
 void Driver::execute_commands(const CommandBucket& commands)
@@ -51,6 +59,70 @@ void Driver::draw_elements_(const Command& command)
     static_cast<const DrawElementsCommand&>(command);
   glDrawElements(GL_TRIANGLES, draw_command.count, draw_command.element_type,
       nullptr);
+}
+
+void Driver::bind_uniform_vec2(const Command& command)
+{
+  assert(command.type == Command::Type::kBindUniformVec2);
+  const BindUniformVec2Command& bind_command =
+    static_cast<const BindUniformVec2Command&>(command);
+  glUniform2fv(bind_command.location, 1, &(bind_command.uniform[0]));
+}
+
+void Driver::bind_uniform_vec3(const Command& command)
+{
+  assert(command.type == Command::Type::kBindUniformVec3);
+  const BindUniformVec3Command& bind_command =
+    static_cast<const BindUniformVec3Command&>(command);
+  glUniform3fv(bind_command.location, 1, &(bind_command.uniform[0]));
+}
+
+void Driver::bind_uniform_vec4(const Command& command)
+{
+  assert(command.type == Command::Type::kBindUniformVec4);
+  const BindUniformVec4Command& bind_command =
+    static_cast<const BindUniformVec4Command&>(command);
+  glUniform4fv(bind_command.location, 1, &(bind_command.uniform[0]));
+}
+
+void Driver::bind_uniform_mat2(const Command& command)
+{
+  assert(command.type == Command::Type::kBindUniformMat2);
+  const BindUniformMat2Command& bind_command =
+    static_cast<const BindUniformMat2Command&>(command);
+  glUniformMatrix2fv(bind_command.location, 1, GL_FALSE, &(bind_command.uniform[0][0]));
+}
+
+void Driver::bind_uniform_mat3(const Command& command)
+{
+  assert(command.type == Command::Type::kBindUniformMat3);
+  const BindUniformMat3Command& bind_command =
+    static_cast<const BindUniformMat3Command&>(command);
+  glUniformMatrix3fv(bind_command.location, 1, GL_FALSE, &(bind_command.uniform[0][0]));
+}
+
+void Driver::bind_uniform_mat4(const Command& command)
+{
+  assert(command.type == Command::Type::kBindUniformMat4);
+  const BindUniformMat4Command& bind_command =
+    static_cast<const BindUniformMat4Command&>(command);
+  glUniformMatrix4fv(bind_command.location, 1, GL_FALSE, &(bind_command.uniform[0][0]));
+}
+
+void Driver::bind_uniform_float(const Command& command)
+{
+  assert(command.type == Command::Type::kBindUniformFloat);
+  const BindUniformFloatCommand& bind_command =
+    static_cast<const BindUniformFloatCommand&>(command);
+  glUniform1f(bind_command.location, bind_command.uniform);
+}
+
+void Driver::bind_uniform_int(const Command& command)
+{
+  assert(command.type == Command::Type::kBindUniformInt);
+  const BindUniformIntCommand& bind_command =
+    static_cast<const BindUniformIntCommand&>(command);
+  glUniform1i(bind_command.location, bind_command.uniform);
 }
 
 }
