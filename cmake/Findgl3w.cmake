dd9384c72c1b96fddc806ec2c4a3bb75e020bb80
Antoine Luciani <antoine1fr@gmail.com>
2021-02-06 20:31:31 -0500

    Stop using gl3w as a submodule
    To complicated on Windows, with Visual Studio. It has to be manually installed from now on.

---
+++ cmake/Findgl3w.cmake
@@ -0,0 +1,33 @@
+find_path(gl3w_INCLUDE_DIR
+  NAMES GL/gl3w.h
+  PATHS "${gl3w_ROOT}/include" "$ENV{gl3w_ROOT}/include"
+)
+find_path(gl3w_SRC_DIR
+  NAMES gl3w.c
+  PATHS "${gl3w_ROOT}/src" "$ENV{gl3w_ROOT}/src"
+)
+
+set(gl3w_VERSION ${PC_gl3w_VERSION})
+mark_as_advanced(gl3w_FOUND gl3w_INCLUDE_DIR gl3w_VERSION)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(gl3w
+  REQUIRED_VARS gl3w_INCLUDE_DIR gl3w_SRC_DIR
+  VERSION_VAR gl3w_VERSION
+)
+
+if(gl3w_FOUND)
+  set(gl3w_INCLUDE_DIRS "${gl3w_INCLUDE_DIR}")
+endif()
+
+if(gl3w_FOUND AND NOT TARGET gl3w::gl3w)
+  add_library(gl3w::gl3w INTERFACE IMPORTED)
+  message("gl3w_INCLUDE_DIRS: ${gl3w_INCLUDE_DIRS}")
+  set_target_properties(gl3w::gl3w PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${gl3w_INCLUDE_DIRS}"
+  )
+  target_sources(gl3w::gl3w
+    INTERFACE
+    "${gl3w_SRC_DIR}/gl3w.c"
+  )
+endif()
