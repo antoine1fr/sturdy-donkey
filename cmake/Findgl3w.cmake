87afcab8b86c9684c93795a1e01737f535704ed8
Antoine Luciani <antoine1fr@gmail.com>
2021-07-06 16:12:56 -0400

    Look for gl3w's library instead of source files

--- cmake/Findgl3w.cmake
+++ cmake/Findgl3w.cmake
@@ -2,9 +2,8 @@ find_path(gl3w_INCLUDE_DIR
   NAMES GL/gl3w.h
   PATHS "${gl3w_ROOT}/include" "$ENV{gl3w_ROOT}/include"
 )
-find_path(gl3w_SRC_DIR
-  NAMES gl3w.c
-  PATHS "${gl3w_ROOT}/src" "$ENV{gl3w_ROOT}/src"
+find_library(gl3w_LIBRARY
+  NAMES gl3w
 )
 
 set(gl3w_VERSION ${PC_gl3w_VERSION})
@@ -12,7 +11,7 @@ mark_as_advanced(gl3w_FOUND gl3w_INCLUDE_DIR gl3w_VERSION)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(gl3w
-  REQUIRED_VARS gl3w_INCLUDE_DIR gl3w_SRC_DIR
+  REQUIRED_VARS gl3w_INCLUDE_DIR gl3w_LIBRARY
   VERSION_VAR gl3w_VERSION
 )
 
@@ -21,12 +20,9 @@ if(gl3w_FOUND)
 endif()
 
 if(gl3w_FOUND AND NOT TARGET gl3w::gl3w)
-  add_library(gl3w::gl3w INTERFACE IMPORTED)
+  add_library(gl3w::gl3w UNKNOWN IMPORTED)
   set_target_properties(gl3w::gl3w PROPERTIES
+    IMPORTED_LOCATION "${gl3w_LIBRARY}"
     INTERFACE_INCLUDE_DIRECTORIES "${gl3w_INCLUDE_DIRS}"
-  )
-  target_sources(gl3w::gl3w
-    INTERFACE
-    "${gl3w_SRC_DIR}/gl3w.c"
   )
 endif()
